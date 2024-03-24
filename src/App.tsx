a617070de8735a888a7ac10169b2879b024b7138
ninjakarate <ilya3aytsev@gmail.com>
2024-03-24 21:50:45 +0200

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/electron-vite.animate.svg'
-import './App.css'
+import { Stack, Typography } from '@mui/material';
 
 function App() {
-  const [count, setCount] = useState(1)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count * count)}>
-          count^2 is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Stack bgcolor={'#ebebeb'}
+      width={'100%'}
+      height={'100vh'}
+      direction={'column'}
+      alignItems={'center'}
+      justifyContent={'center'}
+      spacing={2}
+    >
+      <Typography fontFamily={'Orbitron'} letterSpacing={2}>
+        welcome ?
+      </Typography>
+    </Stack>
   )
 }
 
