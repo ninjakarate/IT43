aa64a12d997031866fb0c57b999898bc0b888039
ninjakarate <ilya3aytsev@gmail.com>
2024-06-30 20:43:04 +0300

    stub for keyboard input layout

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,23 @@ function App() {
       justifyContent={'center'}
       spacing={2}
     >
-      {/* <Typography fontFamily={'Orbitron'} letterSpacing={2}>
-        welcome ?
-      </Typography> */}
+      <Stack
+        position={'absolute'}
+        width={'300px'}
+        height={'150px'}
+        bgcolor={'lightgrey'}
+        borderRadius={2}
+        boxShadow={3}
+        alignItems={'center'}
+        justifyContent={'center'}
+        zIndex={1000}
+        top={'80%'}
+        left={'3%'}
+      >
+        <Typography fontFamily={'Orbitron'} letterSpacing={2}>
+          welcome ?
+        </Typography>
+      </Stack>
       <Raycastin />
     </Stack>
   )
